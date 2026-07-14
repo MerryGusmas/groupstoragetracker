d7ef70227da19bee76a671b53773ca8bc3160bca
angusPhilippi <angusphilippi@gmail.com>
2026-07-15 01:57:00 +1000

    Updated Test files and fixed build issues

--- src/test/java/com/groupstoragetracker/GroupStorageTrackerPluginTest.java
+++ src/test/java/com/groupstoragetracker/GroupStorageTrackerPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.groupstoragetracker;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class GroupStorageTrackerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(GroupStorageTrackerPlugin.class);
 		RuneLite.main(args);
 	}
 }
